250d93366efe30564d5550083fd772eb27ff4b54
deepakparamesh <deepakparamesh007@gmail.com>
2023-12-16 12:10:33 +0530

    push

---
+++ DecoratorPattern/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Com.DeepakParamesh
+{
+
+    // Cloudstream class to store the given data in the cloud
+    public class CloudStream
+    {
+       public void write(string data)
+        {
+            Console.WriteLine("Storing " + data);
+        }
+    }
+
+
+    public class CompressedCloudStream : CloudStream
+    {
+        public void write(string data)
+        {
+            var compressed = compress(data);
+            base.write(data);
+        }
+        private string compress(string data)
+        {
+            return data.Substring(0, 5);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
