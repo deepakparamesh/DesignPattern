77f98af781931c9f0c272838b5220361f4868397
deepakparamesh <deepakparamesh007@gmail.com>
2023-12-17 13:36:44 +0530

    feat(factory): factory pattern implemented

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Com.DeepakParamesh
+{
+    public class Program
+    {
+
+        public interface INotification
+        {
+            void sendNotification();
+        }
+
+        // Concrete classes
+        public class EmailNotification : INotification
+        {
+            public void sendNotification()
+            {
+                Console.WriteLine("Notification sent through Email");
+            }
+        }
+
+        public class SMSNotification : INotification
+        {
+            public void sendNotification()
+            {
+                Console.WriteLine("Notification sent through SMS");
+            }
+        }
+
+        public class MobileNotification : INotification
+        {
+            public void sendNotification()
+            {
+                Console.WriteLine("Notification sent through Mobile");
+            }
+        }
+
+        public class NotificationFactory
+        {
+            public INotification getNotification(string notificationType)
+            {
+                if(notificationType == "EMAIL")
+                {
+                    return new EmailNotification();
+                } else if(notificationType == "SMS")
+                {
+                    return new SMSNotification();
+                } else if(notificationType == "MOBILE")
+                {
+                    return new MobileNotification();
+                } else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static void Main()
+        {
+            NotificationFactory notificationFactory = new();
+
+            INotification notification = notificationFactory.getNotification("MOBILE");
+            notification.sendNotification();
+
+            notification = notificationFactory.getNotification("EMAIL");
+            notification.sendNotification();
+
+            notification = notificationFactory.getNotification("SMS");
+            notification.sendNotification();
+        }
+    }
+}
